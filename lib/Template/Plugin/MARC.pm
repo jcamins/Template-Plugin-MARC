9267d94b7ef44540e5034636c19ad4a4038679b0
Jared Camins-Esakov <jcamins@cpbibliography.com>
2012-12-20 22:06:14 -0500

    Fix some perlcritic issues

--- lib/Template/Plugin/MARC.pm
+++ lib/Template/Plugin/MARC.pm
@@ -138,6 +138,7 @@ use MARC::Field;
 
 use Template::Plugin;
 use base qw( Template::Plugin );
+use Template::Plugin::MARC::Field;
 
 our $VERSION = '0.01';
 
@@ -185,7 +186,7 @@ sub new {
     # marc can be a positional or named argument
     $marc = $config->{'marc'} unless defined $marc;
 
-    bless { 
+    return bless { 
         %$config,
             marc => $marc,
     }, $class;
@@ -265,120 +266,6 @@ sub AUTOLOAD {
     $self->init;
     
     return $self->{'record'}->{"$a"};
-}
-
-1;
-
-=head1 HELPER CLASSES
-
-=cut
-
-package Template::Plugin::MARC::Field;
-
-=head2 Template::Plugin::MARC::Field
-
-Object class to allow nested auto-loading. Not used directly.
-
-=cut
-
-use 5.010000;
-use strict;
-use warnings;
-use MARC::Field;
-
-our $AUTOLOAD;
-
-sub new {
-    my ($class, $field) = @_;
-    my $fieldhash = {
-        'tag' => $field->tag(),
-        'subfields' => [],
-    };
-    if ($field->is_control_field()) {
-        $fieldhash->{'value'} = $field->data();
-            push @{$fieldhash->{'subfields'}}, Template::Plugin::MARC::Subfield->new('@' => $field->data());
-    } else {
-        $fieldhash->{'ind1'} = $field->indicator(1);
-        $fieldhash->{'ind2'} = $field->indicator(2);
-        my @subfields = $field->subfields();
-        foreach my $subf (@subfields) {
-            $fieldhash->{"s$subf->[0]"} = $subf->[1] unless $fieldhash->{"s$subf->[0]"};
-            $fieldhash->{'all'} .= ' ' if $fieldhash->{'all'};
-            $fieldhash->{'all'} .= $subf->[1];
-            push @{$fieldhash->{'subfields'}}, Template::Plugin::MARC::Subfield->new($subf->[0] => $subf->[1]);
-        }
-    }
-
-    bless $fieldhash, $class;
-}
-
-sub has {
-    my ($self, $selector, $match) = @_;
-
-    unless ($selector eq 'ind1' || $selector eq 'ind2' || $selector eq 'tag') {
-        $selector = "s$selector"; # Everything else is a subfield
-    }
-
-    return $self->{$selector} eq $match if (defined $self->{$selector} && defined $match);
-    return defined $self->{$selector};
-}
-
-sub filter {
-    my ($self, $selectors) = @_;
-
-    my $result = '';
-    foreach my $selector (keys %$selectors) {
-        if ($selector eq 'code') {
-            foreach my $subf (@{$self->{'subfields'}}) {
-                if (index($selectors->{$selector}, $subf->code) >= 0) {
-                    $result .= ' ' if $result;
-                    $result .= $subf->value;
-                }
-            }
-        }
-    }
-    return $result;
-}
-
-sub AUTOLOAD {
-    my $self = shift;
-    (my $a = $AUTOLOAD) =~ s/.*:://;
-
-    return $self->{"$a"};
-}
-
-1;
-
-package Template::Plugin::MARC::Subfield;
-
-=head2 Template::Plugin::MARC::Subfield
-
-Object class to allow nested auto-loading. Not used directly.
-
-=cut
-
-
-use 5.010000;
-use strict;
-use warnings;
-
-sub new {
-    my ($class, $code, $value) = @_;
-
-    bless {
-        code => $code,
-        value => $value,
-    }, $class;
-}
-
-sub code {
-    my $self = shift;
-    return $self->{'code'};
-}
-
-sub value {
-    my $self = shift;
-    return $self->{'value'};
 }
 
 1;
